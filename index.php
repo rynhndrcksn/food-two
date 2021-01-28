3a6478a37f47f068b23c0faa9348c142299a18e3
Ryan H <rhendrickson11@mail.greenriver.edu>
2021-01-28 15:42:06 -0500

    made order and order2 use POST. Used a session to hold all the posted information and display it on the summary page.

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+// create a session
+session_start();
+
 // require autoload file
 require_once ('vendor/autoload.php');
 
@@ -31,14 +34,34 @@ $f3->route('GET /order', function() {
 	echo $view->render('views/order.html');
 });
 
+// we can only use POST if the form method is POST, otherwise we need to use GET as GET is used for typing in the
+// URL, hyperlinks, and most other things
 // define an order2 route
-$f3->route('GET /order2', function() {
+$f3->route('POST /order2', function() {
+	// gather info from order
+	if (isset($_POST['food'])) {
+		$_SESSION['food'] = $_POST['food'];
+	}
+	if (isset($_POST['meal'])) {
+		$_SESSION['meal'] = $_POST['meal'];
+	}
 	$view = new Template();
 	echo $view->render('views/order2.html');
 });
 
 // define an order route
-$f3->route('GET /summary', function() {
+$f3->route('POST /summary', function() {
+	// gather info from order2
+	if (isset($_POST['condiments'])) {
+		$_SESSION['condiments'] = implode(', ', $_POST['condiments']);
+	} else {
+		$_SESSION['condiments'] = 'none';
+	}
+
+	echo '<pre>';
+	var_dump($_SESSION);
+	echo '</pre>';
+
 	$view = new Template();
 	echo $view->render('views/summary.html');
 });
